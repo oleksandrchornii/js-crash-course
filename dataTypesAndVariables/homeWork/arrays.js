17d99ef0b1c1a53955e5b6157c3713837e665698
Oleksandr Chornii <oleksandrchorniy2@gmail.com>
2021-11-15 20:31:22 +0200

    first

--- dataTypesAndVariables/homeWork/arrays.js
+++ dataTypesAndVariables/homeWork/arrays.js
@@ -1,5 +1,9 @@
-const arrayNums1 = [7,101,3,1,9,11,100,111];
-let sum = 0;
-for (let i = 0; i < arrayNums1.length; i++) {
-  sum = sum + arrayNums1[i];
-}
+// const arrayNums1 = [7,101,3,1,9,11,100,111];
+// let sum = 0;
+// for (let i = 0; i < arrayNums1.length; i++) {
+//   sum = sum + arrayNums1[i];
+// }
+
+const data = ['Games', {tests: 'e2e',}]
+const [moduleName, extra] = data;
+console.log(data);
